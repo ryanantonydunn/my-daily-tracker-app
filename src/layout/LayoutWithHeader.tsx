90d8ceac89ca3a320715b7471fb3326ede43750d
Ryan Dunn <contact@ryandunn.dev>
2020-07-28 15:56:34 +0100

    add tailwind to layout

--- src/layout/LayoutWithHeader.tsx
+++ src/layout/LayoutWithHeader.tsx
@@ -1,20 +1,18 @@
 import { useNavigation } from "@react-navigation/native";
 import React, { ReactNode, useCallback, useState } from "react";
 import {
+  KeyboardAvoidingView,
+  Platform,
   SafeAreaView,
   StatusBar,
   StyleSheet,
   View,
-  KeyboardAvoidingView,
-  Platform,
 } from "react-native";
-import { gray_200, gray_900, white, green, col } from "../base/colors";
+import { TouchableOpacity } from "react-native-gesture-handler";
 import Dropdown, { DropdownItem } from "../base/Dropdown";
-import IconButton from "../base/IconButton";
-import Box from "./Box";
-import { LinearGradient } from "expo-linear-gradient";
-import ImageRocket from "../images/ImageRocket";
-import UndrawTakingNotes from "../images/UndrawTakingNotes";
+import Gradient from "../base/Gradient";
+import Icon from "../base/Icon";
+import { tw } from "../base/styles/tailwind";
 
 interface LayoutWithHeaderProps {
   title: ReactNode;
@@ -25,22 +23,10 @@ interface LayoutWithHeaderProps {
 }
 
 const styles = StyleSheet.create({
-  header: {
-    backgroundColor: green,
-  },
-  headerInner: {
-    height: 48,
-  },
-  headerSide: {
-    width: 48,
-  },
-  fill: {
-    flex: 1,
-    backgroundColor: white,
-  },
-  content: {
-    flex: 1,
-  },
+  wrapper: tw(`flex-1`),
+  header: tw(`flex-row items-center justify-between h-12`),
+  cell: tw(`w-12 items-center`),
+  iconButton: tw(`p-2`),
 });
 
 const LayoutWithHeader = ({
@@ -58,7 +44,7 @@ const LayoutWithHeader = ({
     ({ children }) =>
       hasKeyboard ? (
         <KeyboardAvoidingView
-          style={{ flex: 1 }}
+          style={styles.wrapper}
           behavior={Platform.OS == "ios" ? "padding" : "height"}
         >
           {children}
@@ -72,46 +58,37 @@ const LayoutWithHeader = ({
   return (
     <Wrapper>
       <StatusBar animated barStyle="light-content" />
-      <SafeAreaView style={styles.header}>
-        <LinearGradient
-          colors={[col("green-5"), col("teal-5")]}
-          start={{ x: 0, y: 1 }}
-          end={{ x: 1, y: 1 }}
-          style={{
-            position: "absolute",
-            left: 0,
-            right: 0,
-            top: 0,
-            bottom: 0,
-          }}
-        />
-        <Box row itemsCenter justifyBetween style={styles.headerInner}>
-          {back ? (
-            <IconButton
-              name="arrow-back"
-              color={gray_200}
-              onPress={() => {
-                if (back === true) {
-                  navigation.goBack();
-                } else if (typeof back === "string") {
-                  navigation.navigate(back);
-                }
-              }}
-            />
-          ) : (
-            <View style={styles.headerSide} />
-          )}
+      <SafeAreaView>
+        <Gradient col1="green-500" col2="teal-500" />
+        <View style={styles.header}>
+          <View style={styles.cell}>
+            {back && (
+              <TouchableOpacity
+                style={styles.iconButton}
+                onPress={() => {
+                  if (back === true) {
+                    navigation.goBack();
+                  } else if (typeof back === "string") {
+                    navigation.navigate(back);
+                  }
+                }}
+              >
+                <Icon name="arrow-back" color="white" />
+              </TouchableOpacity>
+            )}
+          </View>
           {title}
-          {menu?.length ? (
-            <IconButton
-              name="more-vert"
-              color={gray_200}
-              onPress={() => setMenuOpen(true)}
-            />
-          ) : (
-            <View style={styles.headerSide} />
-          )}
-        </Box>
+          <View style={styles.cell}>
+            {menu?.length && (
+              <TouchableOpacity
+                style={styles.iconButton}
+                onPress={() => setMenuOpen(true)}
+              >
+                <Icon name="more-vert" color="white" />
+              </TouchableOpacity>
+            )}
+          </View>
+        </View>
       </SafeAreaView>
       {children}
       <Dropdown
