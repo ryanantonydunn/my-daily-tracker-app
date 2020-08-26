277f2a6efaf18e5326565566b43475f87412acc2
Ryan Dunn <contact@ryandunn.dev>
2020-08-26 15:36:41 +0100

    make delete modal take you back to home

--- src/views/TrackerView.tsx
+++ src/views/TrackerView.tsx
@@ -1,12 +1,15 @@
 import React, { useContext } from "react";
 import { ScrollView } from "react-native-gesture-handler";
+import ConfirmDelete from "../base/ConfirmDelete";
 import T from "../base/Text";
 import LayoutWithHeader from "../layout/LayoutWithHeader";
 import DataContext from "../store/DataContext";
+import UIContext from "../store/UIContext";
 
 const TrackerView = ({ route, navigation }) => {
   const { trackerId } = route.params;
-  const { getTracker } = useContext(DataContext);
+  const { getTracker, deleteTracker } = useContext(DataContext);
+  const { setModal } = useContext(UIContext);
   const tracker = getTracker(trackerId);
 
   const title = tracker?.label || "Unknown Tracker";
@@ -25,7 +28,16 @@ const TrackerView = ({ route, navigation }) => {
           children: "Edit",
         },
         {
-          onPress: () => {},
+          onPress: () =>
+            setModal(
+              <ConfirmDelete
+                cancel={() => setModal()}
+                confirm={() => {
+                  deleteTracker(tracker.id);
+                  navigation.navigate("Home");
+                }}
+              />
+            ),
           children: "Delete",
         },
       ]}
